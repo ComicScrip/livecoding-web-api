04dec8764453d6196b1562f9ed4c4c66f3da3d5b
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-03-19 22:48:36 +0100

    adds pencil and eraser

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const canvas = document.getElementById('drawingZone');
 const ctx = canvas.getContext('2d');
-const backgroundColor = 'red';
 const squareUpperLeftCoords = {
     x: 0,
     y: 0
 }
-const squareColor = 'black';
+const squareColor = '#ff898c';
 const squareSizeInPx = 100;
 const stepInPx = 20;
+const dotRadiusInPx = 5;
+const dotColor = 'white'
+let drawingDot = false;
 
 function drawSquare(x, y, size = squareSizeInPx, color = squareColor) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, size, size);
+}
+
+function drawCircle(x, y, color = dotColor, radius = dotRadiusInPx) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
 }
 
 function updateSquarePosition(dx, dy) {
@@ -20,15 +29,15 @@ function updateSquarePosition(dx, dy) {
 }
 
 function moveSquare(dx, dy) {
-    // clear all the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // clear the previous square
+    ctx.clearRect(squareUpperLeftCoords.x, squareUpperLeftCoords.y, squareSizeInPx, squareSizeInPx);
     // update the position and handle collisions
     updateSquarePosition(dx, dy);
     // redraw
     drawSquare(squareUpperLeftCoords.x, squareUpperLeftCoords.y);
 }
 
-function handleInput(event) {
+function handleKeydown(event) {
     if (event.key === "ArrowUp") {
         moveSquare(0, -stepInPx);
     } else if (event.key === "ArrowDown") {
@@ -40,13 +49,31 @@ function handleInput(event) {
     }
 }
 
+function handleMouseMove(event) {
+    if (drawingDot) {
+        drawCircle(event.clientX, event.clientY);
+    }
+}
+
+function handleMouseDown() {
+    drawingDot = true;
+}
+
+function handleMouseUp() {
+    drawingDot = false;
+}
+
 function init() {
+    // make the canvas fill the whole page
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
     // draw initial rectangle
     drawSquare(squareUpperLeftCoords.x, squareUpperLeftCoords.y);
     // bind event handlers to arrow stokes
-    document.addEventListener('keydown', handleInput)
+    document.addEventListener('keydown', handleKeydown);
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mouseup', handleMouseUp);
 }
 
 init();
